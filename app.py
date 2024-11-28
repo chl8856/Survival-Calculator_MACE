1583fce25c5801ddae15d464bd0f164cad90b654
chl8856 <47550893+chl8856@users.noreply.github.com>
2024-11-28 09:53:39 +0900

    Add files via upload
    initial commit

---
+++ app.py
@@ -0,0 +1,367 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+from model import SQ_survival, SQ_plot
+import pickle
+import os
+
+st.title("Survival Calculator: Major Adverse Cardiovascular Events after Discharge in Patients Underwent Percutaneous Coronary Intervention")
+# st.markdown("need name")
+
+feat_list = [
+    'Age',
+    'BMI',
+    'TC',
+    'HDL',
+    'New_LDL',
+    'TG',
+    'CKMB',
+    'TroponinI',
+    'Creatinine',
+    'MDRD',
+    'HbA1c',
+    'UA',
+    'Hemoglobin',
+    'CRP',
+    'alb_lab',
+    'Fluoro_time',
+    'Contrast_volume',
+    'LVEF',
+    'NRI',
+    'MRCI_K_dis',
+    'MRCI_slop',
+    'Sex',
+    'Antiplatelets',
+    'GP_2b3a',
+    'Statin',
+    'Beta_blocker',
+    'ACEI',
+    'ARB',
+    'Nitrate',
+    'CCB',
+    'Previous_CHF',
+    'HTN',
+    'Previous_CVA',
+    'PAD',
+    'Previous_CRF',
+    'Dialysis',
+    'Dyslipidemia',
+    'COPD',
+    'FHx_Premature_CAD',
+    'AF',
+    'IABP',
+    'RWMA',
+    'LM_disease',
+    'Adm_status_0.0',
+    'Adm_status_1.0',
+    'Adm_status_2.0',
+    'Adm_status_3.0',
+    'ECG_0',
+    'ECG_1',
+    'ECG_2',
+    'ECG_3',
+    'Vascular_access_2_0.0',
+    'Vascular_access_2_1.0',
+    'Vascular_access_2_2.0',
+    'Cath_status_0.0',
+    'Cath_status_1.0',
+    'Cath_status_2.0',
+    'Cath_status_3.0',
+    'VD_0',
+    'VD_1',
+    'VD_2',
+    'VD_3',
+    'TFG_0.0',
+    'TFG_1.0',
+    'TFG_2.0',
+    'TFG_3.0',
+    'Insurance_Cate_1',
+    'Insurance_Cate_2',
+    'Insurance_Cate_3',
+    'Smoking_info_0',
+    'Smoking_info_1',
+    'Smoking_info_2',
+    'Smoking_info_3',
+    'DM_Treat_0',
+    'DM_Treat_1',
+    'DM_Treat_2',
+    'DM_Treat_3',
+    'alcohol_classification_0.0',
+    'alcohol_classification_1.0',
+    'alcohol_classification_2.0',
+    'RCA',
+    'LM',
+    'LAD',
+    'LCx',
+]
+
+num_list = [
+    'Age',
+    'BMI',
+    'TC',
+    'HDL',
+    'New_LDL',
+    'TG',
+    'CKMB',
+    'TroponinI',
+    'Creatinine',
+    'MDRD',
+    'HbA1c',
+    'UA',
+    'Hemoglobin',
+    'CRP',
+    'alb_lab',
+    'Fluoro_time',
+    'Contrast_volume',
+    'LVEF',
+    'NRI',
+    'MRCI_K_dis',
+    'MRCI_slop',
+] 
+
+
+## continous values -> normalize
+## mean value check!
+mapper_yes_no = {
+    "No": 0, 
+    "Yes": 1
+}
+
+mapper_sex = {
+    "Male": 1, 
+    "Female":0
+}
+
+mapper_adm = {
+    "Outpatient department": [1,0,0,0],
+    "Emergency department": [0,1,0,0],
+    "Non-acute transfer": [0,0,1,0],
+    "Acute transfer": [0,0,0,1]
+}
+
+mapper_ecg = {
+    "Normal": [1,0,0,0],
+    "ST depression": [0,1,0,0],
+    "ST elevation": [0,0,1,0],
+    "Nonspecific": [0,0,0,1]
+}
+
+mapper_vascular_access = {
+    "Radial artery": [1,0,0]
+    "Femoral artery": [0,1,0],
+    "Other": [0,0,1]
+}
+
+mapper_vd = {  
+    "No vessle disease": [1,0,0,0],
+    "1 vessle disease": [0,1,0,0],
+    "2 vessle disease": [0,0,1,0],
+    "3 vessle disease": [0,0,0,1]
+}
+
+mapper_tfg = {  
+    "Grade 0": [1,0,0,0],
+    "Grade 1": [0,1,0,0],
+    "Grade 2": [0,0,1,0],
+    "Grade 3": [0,0,0,1]
+}
+
+mapper_insurance = {
+    "NHI": [1,0,0]
+    "Medical aid": [0,1,0],
+    "Other": [0,0,1]
+}
+
+mapper_smoking = {  
+    "Never": [1,0,0,0],
+    "Ex-smoker": [0,1,0,0],
+    "Current smoker": [0,0,1,0],
+    "Unknown": [0,0,0,1]
+}
+
+mapper_dm = {  
+    "None": [1,0,0,0],
+    "Without medication": [0,1,0,0],
+    "With OHA": [0,0,1,0],
+    "With insulin": [0,0,0,1]
+}
+
+mapper_alcohol = {
+    "None": [1,0,0]
+    "Mild": [0,1,0],
+    "Heavy": [0,0,1]
+}
+
+
+
+X_in                     = pd.DataFrame(np.zeros([1, len(feat_list)]), columns=feat_list)
+
+
+st.subheader("Patient Characteristics")
+
+col1 = st.columns(2)
+with col1[0]:
+    age                      = st.slider("Age", min_value=22, max_value=100, value=65, step=1)   
+    bmi                      = st.slider("Body mass index (BMI)", min_value=14.0, max_value=43.0, value=25, step=0.1)
+with col1[1]:
+    sex                      = st.radio("Gender", list(mapper_sex), horizontal=True, index=1)
+    
+X_in['Age'] = age
+X_in['BMI'] = bmi
+X_in['Sex'] = mapper_sex[sex]
+
+
+col2 = st.columns(2)
+with col2[0]:
+    TC                    = st.slider("Total cholesterol, mg/dL", min_value=50, max_value=400, value=170, step=10)   
+    New_LDL               = st.slider("LDL, mg/dL", min_value=11, max_value=300, value=100, step=1)   
+    CKMB                  = st.slider("CKMB, ng/ml", min_value=0, max_value=2000, value=50, step=1)   
+    Creatinine            = st.slider("Creatinine, mg/dL", min_value=0.2, max_value=10.0, value=1.2, step=0.1)
+    HbA1c                 = st.slider("HbA1c, %", min_value=5.0, max_value=15, value=6.5, step=0.5)
+    Hemoglobin            = st.slider("Hemoglobin, g/dL", min_value=6.0, max_value=18.0, value=13.0, step=0.1)   
+    alb_lab               = st.slider("Albumin, g/dL", min_value=2.0, max_value=5.0, value=3.8, step=0.1)
+    Contrast_volume       = st.slider("Contrast volume, ml", min_value=0, max_value=1000, value=250, step=10)   
+    NRI                   = st.slider("Nutritional risk index", min_value=35, max_value=100, value=50, step=5)
+    MRCI_slop             = st.slider("MRCI changed (adm-dis)", min_value=-50, max_value=80, value=3, step=1)       
+with col2[0]:
+    HDL                   = st.slider("HDL, mg/dL", min_value=13, max_value=100, value=45, step=1)   
+    TG                    = st.slider("Triglyceride, mg/dL", min_value=15, max_value=300, value=130, step=1)   
+    TroponinI             = st.slider("Troponin I, ng/ml", min_value=0, max_value=1000, value=20, step=1)
+    MDRD                  = st.slider(r"GFR, ml/min/1.73m$^2$", min_value=5, max_value=300, value=75, step=5)
+    UA                    = st.slider("Uric acid, mg/dL", min_value=1.0, max_value=50.0, value=5.5, step=0.5)
+    CRP                   = st.slider("CRP, mg/dL", min_value=0, max_value=30.0, value=1.5, step=0.5)   
+    Fluoro_time           = st.slider("Fluoro duration, min", min_value=5, max_value=240, value=30, step=5)   
+    LVEF                  = st.slider("LVEF, %", min_value=15, max_value=80, value=60, step=5)
+    MRCI_K_dis            = st.slider("MRCI on discharge", min_value=0, max_value=110, value=24, step=1)   
+
+X_in['TC']                = TC*(400-50)+50
+X_in['HDL']               = HDL*(100-13)+13
+X_in['New_LDL']           = New_LDL*(300-11)+11
+X_in['TG']                = TG*(300-15)+15
+X_in['CKMB']              = CKMB*(2000-0)+0
+X_in['TroponinI']         = TroponinI*(1000-0)+0
+X_in['Creatinine']        = Creatinine*(10.0-0.2)+0.2
+X_in['MDRD']              = MDRD*(300-5)+5
+X_in['HbA1c']             = HbA1c*(15-5)+5
+X_in['UA']                = UA*(50.0-1.0)+1.0
+X_in['Hemoglobin']        = Hemoglobin*(18.0-6.0)+6.0
+X_in['CRP']               = CRP*(30-0)+0
+X_in['alb_lab']           = alb_lab*(5.0-2.0)+2.0
+X_in['Fluoro_time']       = Fluoro_time*(240-5)+5
+X_in['Contrast_volume']   = Contrast_volume*(1000-0)+0
+X_in['LVEF']              = LVEF*(80-15)+15
+X_in['NRI']               = NRI*(100-35)+35
+X_in['MRCI_K_dis']        = MRCI_K_dis*(110-0)+0
+X_in['MRCI_slop']         = MRCI_slop*(80-(-50))+(-50)    
+    
+
+col3 = st.columns(3)
+with col3[0]:
+    Antiplatelets     = st.radio("Antiplatelets", list(mapper_yes_no), horizontal=True, index=0)
+    Beta_blocker      = st.radio("Beta_blocker", list(mapper_yes_no), horizontal=True, index=0)
+    Nitrate           = st.radio("Nitrate", list(mapper_yes_no), horizontal=True, index=0)
+    HTN               = st.radio("Hypertension", list(mapper_yes_no), horizontal=True, index=0)
+    Previous_CRF      = st.radio("Previous_CRF", list(mapper_yes_no), horizontal=True, index=0)
+    COPD              = st.radio("COPD", list(mapper_yes_no), horizontal=True, index=0)
+    IABP              = st.radio("IABP used", list(mapper_yes_no), horizontal=True, index=0)
+with col3[1]:
+    GP_2b3a           = st.radio("GpIIb/IIIa inhibitors", list(mapper_yes_no), horizontal=True, index=0)
+    ACEI              = st.radio("ACEI", list(mapper_yes_no), horizontal=True, index=0)
+    CCB               = st.radio("CCB", list(mapper_yes_no), horizontal=True, index=0)
+    Previous_CVA      = st.radio("Previous_CVA", list(mapper_yes_no), horizontal=True, index=0)
+    Dialysis          = st.radio("Dialysis", list(mapper_yes_no), horizontal=True, index=0)
+    FHx_Premature_CAD = st.radio("FHx_Premature_CAD", list(mapper_yes_no), horizontal=True, index=0)
+    RWMA              = st.radio("RWMA", list(mapper_yes_no), horizontal=True, index=0)
+with col3[2]:
+    Statin            = st.radio("Statin", list(mapper_yes_no), horizontal=True, index=0)
+    ARB               = st.radio("ARB", list(mapper_yes_no), horizontal=True, index=0)
+    Previous_CHF      = st.radio("Previous_CHF", list(mapper_yes_no), horizontal=True, index=0)
+    PAD               = st.radio("Peripheral artery disease", list(mapper_yes_no), horizontal=True, index=0)   
+    Dyslipidemia      = st.radio("Dyslipidemia", list(mapper_yes_no), horizontal=True, index=0)
+    AF                = st.radio("Atrial fibrillation", list(mapper_yes_no), horizontal=True, index=0)       
+    LM_disease        = st.radio("LM_disease", list(mapper_yes_no), horizontal=True, index=0)
+    
+X_in['Antiplatelets'] = mapper_yes_no[Antiplatelets]
+X_in['GP_2b3a'] = mapper_yes_no[GP_2b3a]
+X_in['Statin'] = mapper_yes_no[Statin]
+X_in['Beta_blocker'] = mapper_yes_no[Beta_blocker]
+X_in['ACEI'] = mapper_yes_no[ACEI]
+X_in['ARB'] = mapper_yes_no[ARB]
+X_in['Nitrate'] = mapper_yes_no[Nitrate]
+X_in['CCB'] = mapper_yes_no[CCB]
+X_in['Previous_CHF'] = mapper_yes_no[Previous_CHF]
+X_in['HTN'] = mapper_yes_no[HTN]
+X_in['Previous_CVA'] = mapper_yes_no[Previous_CVA]
+X_in['PAD'] = mapper_yes_no[PAD]
+X_in['Previous_CRF'] = mapper_yes_no[Previous_CRF]
+X_in['Dialysis'] = mapper_yes_no[Dialysis]
+X_in['Dyslipidemia'] = mapper_yes_no[Dyslipidemia]
+X_in['COPD'] = mapper_yes_no[COPD]
+X_in['FHx_Premature_CAD'] = mapper_yes_no[FHx_Premature_CAD]
+X_in['AF'] = mapper_yes_no[AF]
+X_in['IABP'] = mapper_yes_no[IABP]
+X_in['RWMA'] = mapper_yes_no[RWMA]
+X_in['LM_disease'] = mapper_yes_no[LM_disease]
+
+
+col4 = st.columns(1)
+with col4[0]:
+    Adm_statu               = st.radio("Adm_status", list(mapper_adm), horizontal=True, index=0)
+    ECG                     = st.radio("ECG", list(mapper_ecg), horizontal=True, index=0)
+    Vascular_access         = st.radio("Access route", list(mapper_vascular_access), horizontal=True, index=0)
+    Cath_status             = st.radio("Cath_status", list(mapper_cath_status), horizontal=True, index=0)
+    VD                      = st.radio("Severity CAD", list(mapper_vd), horizontal=True, index=0)
+    TFG                     = st.radio("TIMI grade flow", list(mapper_tfg), horizontal=True, index=0)
+    Insurance_Cate          = st.radio("Insurance type", list(mapper_insurance), horizontal=True, index=0)
+    Smoking_info            = st.radio("Smoking status", list(mapper_smoking), horizontal=True, index=0)
+    DM_Treat                = st.radio("Diabetes mellitus", list(mapper_dm), horizontal=True, index=0)
+    alcohol_classification  = st.radio("Alcohol consumption ", list(mapper_alcohol), horizontal=True, index=0)
+    
+X_in[['Adm_status_0.0', 'Adm_status_1.0', 'Adm_status_2.0', 'Adm_status_3.0']] = mapper_adm[Adm_status]
+X_in[['ECG_0','ECG_1','ECG_2','ECG_3']] = mapper_ecg[ECG]
+X_in[['Vascular_access_2_0.0', 'Vascular_access_2_1.0', 'Vascular_access_2_2.0']] = mapper_vascular_access[Vascular_access]
+X_in[['Cath_status_0.0', 'Cath_status_1.0', 'Cath_status_2.0', 'Cath_status_3.0']] = mapper_cath_status[Cath_status]
+X_in[['VD_0', 'VD_1', 'VD_2', 'VD_3']] = mapper_vd[VD]
+X_in[['TFG_0.0', 'TFG_1.0', 'TFG_2.0', 'TFG_3.0']] = mapper_tfg[TFG]
+X_in[['Insurance_Cate_1', 'Insurance_Cate_2', 'Insurance_Cate_3']] = mapper_insurance[Insurance_Cate]
+X_in[['Smoking_info_0', 'Smoking_info_1', 'Smoking_info_2', 'Smoking_info_3']] = mapper_smoking[Smoking_info]
+X_in[['DM_Treat_0', 'DM_Treat_1', 'DM_Treat_2', 'DM_Treat_3']] = mapper_dm[DM_Treat]
+X_in[['alcohol_classification_0.0', 'alcohol_classification_1.0', 'alcohol_classification_2.0']] = mapper_alcohol[alcohol_classification]
+
+    
+col5 = st.columns(2)
+with col5[0]:
+    RCA               = st.radio("PCI-RCA territory", list(mapper_yes_no), horizontal=True, index=0)
+    LAD               = st.radio("PCI-LAD territory", list(mapper_yes_no), horizontal=True, index=0)
+
+with col5[1]:
+    LM                = st.radio("PCI-LM", list(mapper_yes_no), horizontal=True, index=0)
+    LCx               = st.radio("PCI-LCx territory", list(mapper_yes_no), horizontal=True, index=0)
+
+X_in['RCA'] = mapper_yes_no[RCA]
+X_in['LM'] = mapper_yes_no[LM]
+X_in['LAD'] = mapper_yes_no[LAD]
+X_in['LCx'] = mapper_yes_no[LCx]
+
+
+st.subheader(" ")
+
+BUTTON = st.button("Predict Risk")
+
+
+def update_risk_score(x):
+    survival_ = SQ_survival(x) #percentage
+
+    # data = "Predicted Survival Probability: {:.2f}%".format(risk_)
+    data = "Predicted Survival Probability: {}%".format(survival_)
+
+    return data
+
+
+if BUTTON:
+    # c = st.container()
+    fig = SQ_plot(X_in)
+    # with c: 
+    st.plotly_chart(fig, theme=None, use_container_width=True)
+        # st.plotly_chart(fig, theme="streamlit", use_container_width=True)
---
+++ model.py
@@ -0,0 +1,214 @@
+import numpy as np
+import pandas as pd
+import plotly.graph_objs as go
+
+import warnings
+import joblib
+
+
+
+warnings.filterwarnings('ignore')
+
+def get_ensemble_prediction(models, W, X, time_horizons):
+    # print('conducting ensemble prediction...')
+    time_optimization = [30, 60, 90, 180, 270, 365]
+    
+    time_horizons_superset = np.unique(time_optimization + time_horizons).astype(int).tolist()
+
+    pred = np.zeros([len(X), len(time_horizons_superset)])
+
+    for m_idx in range(len(models)):
+        # print('ing...' + models[m_idx].name)
+        tmp_pred1 = models[m_idx].predict(X, time_horizons_superset)
+        tmp_pred2 = models[m_idx].predict(X, time_optimization)
+
+        for tt in range(len(time_optimization)):
+            if tt == 0:
+                tmp_time_idx1 = np.asarray(time_horizons_superset) <= time_optimization[tt]
+                tmp_time_idx2 = np.asarray(time_horizons_superset) >  time_optimization[tt]
+
+                prev_val      = np.zeros([len(X), 1])
+                next_val      = tmp_pred2[:, [tt]]
+
+                increment              = tmp_pred1[:, tmp_time_idx1] - prev_val
+                pred[:, tmp_time_idx1] = pred[:, tmp_time_idx1] + W[tt,m_idx] * increment                
+                pred[:, tmp_time_idx2] = pred[:, tmp_time_idx2] + W[tt,m_idx] * (next_val - prev_val)
+
+            elif tt == len(time_optimization) - 1: #the last index  
+                # tmp_time_idx1 = (np.asarray(time_horizons_superset) > self.time_optimization[tt-1]) & (np.asarray(time_horizons_superset) <= self.time_optimization[tt])
+                tmp_time_idx1 = (np.asarray(time_horizons_superset) > time_optimization[tt-1])
+                prev_val      = tmp_pred2[:, [tt-1]]
+
+                increment              = tmp_pred1[:, tmp_time_idx1] - prev_val
+                pred[:, tmp_time_idx1] = pred[:, tmp_time_idx1] + W[tt,m_idx] * increment                
+
+            else:
+                tmp_time_idx1 = (np.asarray(time_horizons_superset) > time_optimization[tt-1]) & (np.asarray(time_horizons_superset) <= time_optimization[tt])
+                tmp_time_idx2 = np.asarray(time_horizons_superset) >  time_optimization[tt]
+                prev_val      = tmp_pred2[:, [tt-1]]
+                next_val      = tmp_pred2[:, [tt]]
+
+                increment              = tmp_pred1[:, tmp_time_idx1] - prev_val
+                pred[:, tmp_time_idx1] = pred[:, tmp_time_idx1] + W[tt,m_idx] * increment                
+                pred[:, tmp_time_idx2] = pred[:, tmp_time_idx2] + W[tt,m_idx] * (next_val - prev_val)
+
+    return pred[:, [f_idx for f_idx, f in enumerate(time_horizons_superset) if f in time_horizons]]
+
+
+
+eval_times = np.arange(1,366).tolist()
+
+surv_avg   = np.asarray([[0.99920442, 0.99882281, 0.99882281, 0.99801285, 0.99762202,
+       0.99723514, 0.99684378, 0.99606775, 0.99565234, 0.99525677,
+       0.99525677, 0.99525677, 0.99487729, 0.99447603, 0.99447603,
+       0.99447603, 0.99447603, 0.99447603, 0.99407075, 0.99367498,
+       0.99325792, 0.99325792, 0.99286347, 0.99247076, 0.99247076,
+       0.99247076, 0.99247076, 0.99119663, 0.99119663, 0.99119663,
+       0.9907922 , 0.9907922 , 0.9907922 , 0.9907922 , 0.99038756,
+       0.98998275, 0.98998275, 0.98998275, 0.98998275, 0.98998275,
+       0.98998275, 0.98998275, 0.98957718, 0.98836113, 0.98795214,
+       0.98795214, 0.98754325, 0.98754325, 0.98672293, 0.98672293,
+       0.98672293, 0.98672293, 0.985902  , 0.985902  , 0.98549048,
+       0.98549048, 0.98549048, 0.98549048, 0.98549048, 0.98507908,
+       0.98466745, 0.98384455, 0.98343274, 0.98260907, 0.98260907,
+       0.98260907, 0.98260907, 0.98219523, 0.98219523, 0.98136798,
+       0.9809541 , 0.9809541 , 0.9809541 , 0.9809541 , 0.98012569,
+       0.98012569, 0.98012569, 0.98012569, 0.98012569, 0.97971145,
+       0.97971145, 0.9792955 , 0.97887949, 0.97887949, 0.97846322,
+       0.97804691, 0.97804691, 0.97721395, 0.97679739, 0.97679739,
+       0.97679739, 0.97679739, 0.97679739, 0.97638028, 0.97596324,
+       0.97596324, 0.97596324, 0.97596324, 0.97596324, 0.97596324,
+       0.97554585, 0.97554585, 0.97554585, 0.97554585, 0.97512744,
+       0.97512744, 0.97512744, 0.97470894, 0.97470894, 0.97429055,
+       0.97429055, 0.97429055, 0.97387207, 0.97387207, 0.97387207,
+       0.97387207, 0.97387207, 0.97387207, 0.97387207, 0.97387207,
+       0.97345151, 0.97303004, 0.97303004, 0.97303004, 0.97260855,
+       0.97218606, 0.97218606, 0.97218606, 0.97218606, 0.97133937,
+       0.97133937, 0.97091598, 0.97091598, 0.97091598, 0.97091598,
+       0.97091598, 0.97091598, 0.97091598, 0.97091598, 0.97049255,
+       0.97049255, 0.97049255, 0.97049255, 0.97049255, 0.97049255,
+       0.97049255, 0.97049255, 0.97049255, 0.97049255, 0.97049255,
+       0.97049255, 0.96964237, 0.96964237, 0.9692166 , 0.9692166 ,
+       0.9692166 , 0.9692166 , 0.96879005, 0.96879005, 0.96836359,
+       0.96836359, 0.96836359, 0.96836359, 0.96836359, 0.96836359,
+       0.96793555, 0.96793555, 0.96750684, 0.96750684, 0.96707786,
+       0.96707786, 0.96622035, 0.96579113, 0.96579113, 0.96536201,
+       0.96536201, 0.96536201, 0.96536201, 0.96536201, 0.96450368,
+       0.96450368, 0.96450368, 0.96450368, 0.96450368, 0.96450368,
+       0.96450368, 0.96450368, 0.96364326, 0.96322224, 0.96322224,
+       0.96322224, 0.96322224, 0.96236208, 0.96236208, 0.96236208,
+       0.96236208, 0.96236208, 0.96236208, 0.9619304 , 0.9619304 ,
+       0.9619304 , 0.96150181, 0.96150181, 0.96150181, 0.96150181,
+       0.96150181, 0.96150181, 0.9610736 , 0.9610736 , 0.9610736 ,
+       0.9610736 , 0.9610736 , 0.9610736 , 0.96064212, 0.96064212,
+       0.96064212, 0.96020591, 0.96020591, 0.96020591, 0.96020591,
+       0.96020591, 0.95977599, 0.95977599, 0.95891339, 0.95891339,
+       0.95891339, 0.95891339, 0.95891339, 0.95891339, 0.95891339,
+       0.95891339, 0.95891339, 0.95891339, 0.95891339, 0.95891339,
+       0.95848693, 0.95718753, 0.9567572 , 0.955895  , 0.955895  ,
+       0.955895  , 0.95503546, 0.95460463, 0.95417129, 0.95417129,
+       0.95373727, 0.95330187, 0.95286775, 0.95286775, 0.95286775,
+       0.95286775, 0.95286775, 0.95286775, 0.95286775, 0.95286775,
+       0.95199557, 0.95068672, 0.95068672, 0.94937483, 0.94894025,
+       0.94894025, 0.94849832, 0.94761456, 0.94628343, 0.94452321,
+       0.94320351, 0.94276339, 0.94232553, 0.94055912, 0.9401185 ,
+       0.93836445, 0.93616764, 0.93396882, 0.93265403, 0.93132619,
+       0.93088446, 0.92999227, 0.92821112, 0.92732199, 0.92510106,
+       0.92376989, 0.92376989, 0.92243559, 0.92110904, 0.92022173,
+       0.91889114, 0.91755914, 0.91533768, 0.91489012, 0.91310289,
+       0.91310289, 0.91221308, 0.91176918, 0.91087741, 0.90776636,
+       0.90688284, 0.90688284, 0.90643342, 0.90643342, 0.90554417,
+       0.9046549 , 0.90421555, 0.90376951, 0.90287845, 0.90198672,
+       0.90198672, 0.90109351, 0.90109351, 0.90109351, 0.90109351,
+       0.90109351, 0.90109351, 0.89974232, 0.89885619, 0.89885619,
+       0.89885619, 0.89885619, 0.89795219, 0.89704693, 0.89524645,
+       0.89524645, 0.89524645, 0.89524645, 0.89479378, 0.89434219,
+       0.89389273, 0.89298449, 0.89253579, 0.89208742, 0.89208742,
+       0.89208742, 0.89208742, 0.89208742, 0.89073857, 0.89073857,
+       0.89028572, 0.89028572, 0.88983528, 0.88848167, 0.88848167,
+       0.88848167, 0.88848167, 0.88848167, 0.88802631, 0.88756859,
+       0.88756859, 0.88756859, 0.88711117, 0.88711117, 0.88620149,
+       0.88620149, 0.88620149, 0.88620149, 0.88620149, 0.88620149,
+       0.88620149, 0.88620149, 0.88529044, 0.88529044, 0.88483664,
+       0.88483664, 0.88483664, 0.88438559, 0.88393476, 0.88393476]])
+
+def SQ_survival(x):
+
+#     models_in = []
+#     for m in range(7):
+#         models_in += [joblib.load('./saved/model{}.joblib'.format(m))]
+
+#     W_in      = np.array(
+#         [[0.17677209, 0.11356408, 0.70966381],
+#          [0.26585542, 0.        , 0.73414458],
+#          [0.26585542, 0.        , 0.73414458],
+#          [0.26585542, 0.        , 0.73414458],
+#          [0.00467423, 0.03682702, 0.95849874],
+#          [0.00467423, 0.03682702, 0.95849874]])
+        
+#     pred      = get_ensemble_prediction(models_in, W_in, x, eval_times)
+        
+    model = joblib.load('./saved/trained_SQ.joblib')
+    pred  = model.predict(X_te, time_horizons=eval_times)
+
+    return 1. - pred
+
+
+def SQ_plot(x):
+    surv_s     = SQ_survival(x)
+
+    tmp_df = pd.DataFrame(
+        np.concatenate([
+            np.asarray([eval_times]), 
+            np.round(surv_avg, 4),
+            np.round(surv_s, 4)
+        ], axis=0).T,
+        columns = ['Time', 'Avg', 'Yours']
+    )
+
+    fig = go.Figure([
+        go.Scatter(
+            name='Avg.',
+            x=tmp_df['Time'],
+            y=tmp_df['Avg'],
+            mode='lines',
+            line=dict(color='royalblue', width=2, dash='dash'),
+        ),
+        go.Scatter(
+            name='Yours',
+            x=tmp_df['Time'],
+            y=tmp_df['Yours'],
+            mode='lines',
+            line=dict(color='crimson', width=2.5,),
+        ), 
+    ])
+
+    fig.update_layout(
+        xaxis_title='Time (day)',
+        yaxis_title='Survival Probability',
+        # title='Survival Probability',
+        hovermode="x",
+        yaxis=dict(
+            range=[0., 1.]
+        ),
+        xaxis=dict(
+            range=[1., 365.]
+        ),
+        font=dict(
+            size=14
+        ),
+        height=400,
+        margin=dict(
+            l=50, #left margin
+            r=0, #right margin
+            b=50, #bottom margin
+            t=30, #top margin)
+        )
+        # margin=dict(
+        #     l=10, #left margin
+        #     r=5, #right margin
+        #     b=10, #bottom margin
+        #     t=15, #top margin)
+        # )
+    )
+    return fig
